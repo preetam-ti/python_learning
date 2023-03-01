d41bc223eb4442bc704ec8cb3b613fc6327ba980
preetam-ti <preetam.chandrawat@telusinternational.com>
2023-03-01 14:24:29 +0530

    changes

--- conditional_operator.py
+++ conditional_operator.py
@@ -65,3 +65,13 @@ if x is y:
 # is not
 if x is not y:
   print(x is not y)
+
+
+##If with IN
+movies_watched = {"The Matrix", "Green Book", "Her"}
+user_movie = input("Enter something you've watched recently: ")
+
+if user_movie in movies_watched:
+    print(f"I've watched {user_movie} too!")
+else:
+    print("I haven't watched that yet.")
